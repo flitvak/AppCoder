0c7561ae1778e707317f4cce764cbb465f94368f
Federico Litvak <federicolitvak@yahoo.com.ar>
2021-12-12 18:37:58 -0300

    Hasta clase 18 del curso Python

---
+++ ProyectoCoder/AppCoder/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.db.models.fields import EmailField
+
+# Create your models here.
+
+# ACA ES DONDE SE GENERAN LAS CLASES
+
+class Curso(models.Model):
+
+    nombre = models.CharField (max_length=40)
+    comision = models.IntegerField()
+
+class Estudiante(models.Model):
+    
+    nombre = models.CharField(max_length=40)
+    apeliido = models.CharField (max_length=30)
+    email = models.EmailField()
+
+class Profesor (models.Model):
+    nombre = models.CharField (max_length=40)
+    apellido = models.CharField (max_length=40)
+    email = models.EmailField()
+    profesion = models.CharField (max_length=40)
+
+class Entregable (models.Model):
+    nombre = models.CharField (max_length=40)
+    fechaDelEntregable = models.DateField()
+    entregado = models.BooleanField()   
