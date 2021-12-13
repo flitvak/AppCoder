67add4e41016b86b65de9f29aca59723f74f862a
Federico Litvak <federicolitvak@yahoo.com.ar>
2021-12-12 22:48:18 -0300

    Clase 20 hasta break - Se agrego el menu

--- ProyectoCoder/AppCoder/urls.py
+++ ProyectoCoder/AppCoder/urls.py
@@ -4,10 +4,10 @@ from AppCoder import views
 
 urlpatterns = [
    
-    path('inicio', views.inicio),
+    path('inicio', views.inicio, name="Inicio"),
     path('cursos', views.cursos, name ="Cursos"),
-    path('profesores', views.profesores),
-    path('estudiantes', views.estudiantes),
-    path('entregables', views.entregables),
+    path('profesores', views.profesores, name="Profesores"),
+    path('estudiantes', views.estudiantes, name ="Estudiantes"),
+    path('entregables', views.entregables, name ="Entregables"),
 
 ]
