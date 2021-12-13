1088a1c79e31df21c274be0e4cdc517a412c6a92
Federico Litvak <federicolitvak@yahoo.com.ar>
2021-12-12 22:07:28 -0300

    Hasta final de clase 19 V2

--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -10,11 +10,17 @@ class Curso(models.Model):
     nombre = models.CharField (max_length=40)
     comision = models.IntegerField()
 
+    def __str__(self):
+        return f"Curso: {self.nombre} Comisión: {self.comision}" # Esto es para que en el panel de Admin aparezca en texto claro en lugar de que diga Object X
+
 class Estudiante(models.Model):
     
     nombre = models.CharField(max_length=40)
     apeliido = models.CharField (max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} Apellido: {self.apellido} E-mail: {self.email}"
 
 class Profesor(models.Model):
     nombre = models.CharField (max_length=40)
@@ -22,7 +28,14 @@ class Profesor(models.Model):
     email = models.EmailField()
     profesion = models.CharField (max_length=40)
 
+    def __str__(self):
+        return f"Nombre: {self.nombre} Apellido: {self.apellido} E-mail: {self.email} Profesión: {self.profesion}"
+
+
 class Entregable(models.Model):
     nombre = models.CharField (max_length=40)
     fechaDelEntregable = models.DateField()
     entregado = models.BooleanField()   
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} Fecha del entregable: {self.fechaDelEntregable} Entregado: {self.entregado}"
