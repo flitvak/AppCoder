6988bea3451c9019511e6276cbff55f808a512af
Federico Litvak <federicolitvak@yahoo.com.ar>
2021-12-12 21:50:09 -0300

    Hasta final de clase 19

--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -16,13 +16,13 @@ class Estudiante(models.Model):
     apeliido = models.CharField (max_length=30)
     email = models.EmailField()
 
-class Profesor (models.Model):
+class Profesor(models.Model):
     nombre = models.CharField (max_length=40)
     apellido = models.CharField (max_length=40)
     email = models.EmailField()
     profesion = models.CharField (max_length=40)
 
-class Entregable (models.Model):
+class Entregable(models.Model):
     nombre = models.CharField (max_length=40)
     fechaDelEntregable = models.DateField()
     entregado = models.BooleanField()   
